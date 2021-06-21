6e21151c6153313735324ee3d105cce66a7c7235
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2021-06-21 13:59:03 +0700

    update navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,7 @@ function Nav() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <nav className="fixed w-full z-50 bg-white text-gray-900 dark:text-white dark:bg-gray-900 shadow">
+      <nav className="fixed w-full z-50 bg-white text-gray-900 dark:text-white dark:bg-gray-900 bg-opacity-80 dark:bg-opacity-80 backdrop-filter backdrop-blur-lg shadow">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex-shrink-0">
@@ -43,11 +43,11 @@ function Nav() {
 
               <ThemeToggle />
 
-              <div className="-mr-2 flex bg-white items-center dark:bg-gray-900 md:hidden">
+              <div className="-mr-2 flex items-center md:hidden">
                 <button
                   onClick={() => setIsOpen(!isOpen)}
                   type="button"
-                  className="inline-flex items-center ml-2 justify-center p-2 rounded-md text-gray-800 dark:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                  className="inline-flex items-center ml-2 justify-center p-2 rounded-md text-gray-800 dark:text-gray-100 focus:outline-none"
                   aria-controls="mobile-menu"
                   aria-expanded="false"
                 >
@@ -100,35 +100,30 @@ function Nav() {
           leaveFrom="opacity-100 scale-100"
           leaveTo="opacity-0 scale-95"
         >
-          {(ref) => (
-            <div
-              className="md:hidden bg-white dark:bg-gray-900 shadow "
-              id="mobile-menu"
-            >
-              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                <Link
-                  to="/about"
-                  className="block px-3 py-2 rounded-md text-lg no-underline"
-                >
-                  About
-                </Link>
+          <div className="md:hidden" id="mobile-menu">
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              <Link
+                to="/about"
+                className="block px-3 py-2 rounded-md text-lg no-underline"
+              >
+                About
+              </Link>
 
-                <Link
-                  to="/blog"
-                  className="block px-3 py-2 rounded-md text-lg no-underline"
-                >
-                  Blog
-                </Link>
+              <Link
+                to="/blog"
+                className="block px-3 py-2 rounded-md text-lg no-underline"
+              >
+                Blog
+              </Link>
 
-                <Link
-                  to="/reads"
-                  className="block px-3 py-2 rounded-md text-lg no-underline"
-                >
-                  Reading List
-                </Link>
-              </div>
+              <Link
+                to="/reads"
+                className="block px-3 py-2 rounded-md text-lg no-underline"
+              >
+                Reading List
+              </Link>
             </div>
-          )}
+          </div>
         </Transition>
       </nav>
     </div>
