ce59789a97365955524a79d29b82684795cda856
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2021-06-26 13:51:08 +0700

    add active state

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,6 +4,18 @@ import { Link } from "gatsby";
 import ThemeToggle from "./themetoggle";
 import { debounce, throttle } from "throttle-debounce";
 
+const NavLink = ({ to, text }) => {
+  return (
+    <Link
+      to={to}
+      className="py-2 my-2 rounded px-2 mx-2 text-lg no-underline bg-transparent"
+      activeClassName="bg-pink-200 dark:bg-pink-900 dark:text-white text-black"
+    >
+      {text}
+    </Link>
+  );
+};
+
 function Nav() {
   const [isOpen, setIsOpen] = useState(false);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
@@ -40,33 +52,16 @@ function Nav() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex-shrink-0">
-              <Link to="/" className="py-2 rounded-md text-lg no-underline">
+              <Link to="/" className="py-2 rounded-md text-xl no-underline">
                 Home
               </Link>
             </div>
             <div className="flex items-center justify-end">
               <div className="hidden md:block mr-4">
-                <div className="ml-10 flex items-baseline space-x-4">
-                  <Link
-                    to="/about"
-                    className=" px-3 py-2 rounded-md  text-lg no-underline"
-                  >
-                    About
-                  </Link>
-
-                  <Link
-                    to="/blog"
-                    className=" px-3 py-2 rounded-md  text-lg no-underline"
-                  >
-                    Posts
-                  </Link>
-
-                  <Link
-                    to="/reads"
-                    className=" px-3 py-2 rounded-md text-lg no-underline"
-                  >
-                    Reading List
-                  </Link>
+                <div className="ml-10 flex">
+                  <NavLink to="/about" text="About" />
+                  <NavLink to="/blog" text="Posts" />
+                  <NavLink to="/reads" text="Reading List" />
                 </div>
               </div>
 
@@ -130,27 +125,10 @@ function Nav() {
           leaveTo="opacity-0 scale-95"
         >
           <div className="md:hidden" id="mobile-menu">
-            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              <Link
-                to="/about"
-                className="block px-3 py-2 rounded-md text-lg no-underline"
-              >
-                About
-              </Link>
-
-              <Link
-                to="/blog"
-                className="block px-3 py-2 rounded-md text-lg no-underline"
-              >
-                Posts
-              </Link>
-
-              <Link
-                to="/reads"
-                className="block px-3 py-2 rounded-md text-lg no-underline"
-              >
-                Reading List
-              </Link>
+            <div className="px-4 pt-2 pb-3 space-y-1 flex flex-col">
+              <NavLink to="/about" text="About" />
+              <NavLink to="/blog" text="Posts" />
+              <NavLink to="/reads" text="Reading List" />
             </div>
           </div>
         </Transition>
